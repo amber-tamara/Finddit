184c866b23f02a61549b320f00b2a29c8e694437
Amber Tamara <ambertjansen@gmail.com>
2020-01-15 22:27:03 +0000

    Finddit

---
+++ index.js
@@ -0,0 +1,7 @@
+const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("search-input");
+
+searchForm.addEventListener("submit", e => {
+  console.log(123);
+  e.preventDefault();
+});
